0ffd12c45dfe2548fd438e53b5b6317a048dc3ec
Michał Frontczak <77734214+mfrontczak@users.noreply.github.com>
2022-04-02 09:05:38 +0200

    Update Example4.java

--- materiały/Lab1/Example4.java
+++ materiały/Lab1/Example4.java
@@ -9,10 +9,10 @@ public class Petle {
         }
         
         i = 0;
-		do {
-			System.out.println(i);
-			i = i + 1;
-		} while (i <= 10);
+        do {
+            System.out.println(i);
+            i = i + 1;
+        } while (i <= 10);
 
         for(int j = 0; j < 10; j++) {
             System.out.println(j);
