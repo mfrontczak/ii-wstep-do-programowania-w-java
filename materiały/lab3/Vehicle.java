37c518ed66d44d6ec577001f5ad5c918b74add91
Michał Frontczak <77734214+mfrontczak@users.noreply.github.com>
2022-04-05 09:29:40 +0200

    Create Vehicle.java

---
+++ materiały/lab3/Vehicle.java
@@ -0,0 +1,13 @@
+package pl.krakow.up;
+
+public class Vehicle {
+    protected String make;
+    protected String model;
+    protected String plateNo;
+
+    public Vehicle(String make, String model, String plateNo) {
+        this.make = make;
+        this.model = model;
+        this.plateNo = plateNo;
+    }
+}
