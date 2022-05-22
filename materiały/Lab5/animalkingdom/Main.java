ca844cb5c8440fca0287dd009dff4a2c5332e76e
Michał Frontczak <77734214+mfrontczak@users.noreply.github.com>
2022-05-22 12:27:25 +0200

    Update Main.java

--- materiały/Lab5/animalkingdom/Main.java
+++ materiały/Lab5/animalkingdom/Main.java
@@ -4,9 +4,9 @@ public class Main {
     public static void main(String[] args) {
         AnimalKingdom<Seal> sealsKingdom = new AnimalKingdom<>();
 
-        aquaManAnimalKingdom.addAnimal(
+        sealsKingdom.addAnimal(
                 new Seal(85, "Foka", Animal.REGION.OCEAN));
 
-        aquaManAnimalKingdom.showAnimals();
+        sealsKingdom.showAnimals();
     }
 }
